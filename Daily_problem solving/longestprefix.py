63eddb2a0f3a283436d5153d9aa48b2faca87afe
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-01-23 19:10:00 +0000

    Add files via upload

--- Daily_problem solving/longestprefix.py
+++ Daily_problem solving/longestprefix.py
@@ -1,34 +1,61 @@
 
-def longest_prefix(s):
-    '''
-    return the longest prefix between two words
-    '''
-    if len(s) == 1:
-        return s[0]
-    if len(s) == 0:
+def log(n):
+    pref = ""
+    if len(n) == 0:
         return ""
-    a = s[0]
-    b = s[1]
-    if a[0] != b[0]:
-        return ""
-    elif len(a)== 1 or len(b) == 1:
-        if a[0] == b[0]:
-            return a[0]
-    elif len(a)== 0 or len(b) == 0:
-        return ""
-    else:
-            pref = s[0][0]
-            s[0] = a[1:]
-            s[1] = b[1:]
-            pref = pref+longest_prefix(s)
-            
-            if len(s)> 2:
-                next_pref = next_pref+longest_prefix(s[1:])
-    
-                return next_pref
-            
+    if len(n) == 1:
+        return n[0]
+    for i in range(1,len(n)):
+        n[i] = longest_prefix(n[i-1],n[i])
+    return n[i]
+          
+def longest_prefix(a,b):
+            '''
+            return the longest prefix between two words
+            '''
+            if len(a)== 0 or len(b) == 0:
+                return ""
+            elif a[0] != b[0]:
+                return ""
+            elif len(a)== 1 or len(b) == 1:
+                if a[0] == b[0]:
+                    return a[0]
+            elif len(a)== 0 or len(b) == 0:
+                return ""
             else:
-                return  pref
+                    pref = a[0]
+                    pref = pref+longest_prefix(a[1:],b[1:])
+                    return pref
+        
+        
+        
+                    
+            
+
+# second approach:
+
+def longest_pref(strs):
+     pref = ""
+     for new in zip(*strs):
+         if len(set(new)) == 1:
+             pref += new[0]
+         else:
+             return pref
+     return pref
+
+# third approach alternative
+def long(strs):
+        pref = strs[0]
+        for i in strs:
+            while not i.startswith(pref):
+                pref = pref[:-1]
+        return pref
+                
+             
+        
+     
+
+
                     
            
        
@@ -37,5 +64,8 @@ def longest_prefix(s):
        
                             
             
-strs = ["racogg", "rat", "rnime", "c", "v"]
-print(longest_prefix(strs))
+strs = ["eddy", "eddy", "edmond", "e"]
+#print(longest_pref(strs))
+#print(long(strs))
+print(log(strs))
+#print(longest_prefix(strs))
