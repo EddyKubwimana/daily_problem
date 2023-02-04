24ce1287b167d87728b2299aa927ceac3db0af6b
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-02-04 23:24:19 +0000

    Add files via upload

--- Daily_problem solving/find_close_summ.py
+++ Daily_problem solving/find_close_summ.py
@@ -1,9 +1,20 @@
+
+''' This program finds a closest sum of three number to a given target
+@author : Eddy Kubwimana
+@email: eddy.kubwimana@ashesi.edu.gh
+@created: 04/4/2023
+@version : 1:0:1
+'''
+
+# function definiton for the whole process
 def closer(nums, target):
+    '''
+    Takes a list of numbers and a target
+    returns the close sum of triplet
+    '''
     summ = []
-    x = True
     counter = 0
-    while x is True:
-            
+    while True: 
         for i in range (1,len(nums)):
             if i<=len(nums)+2:
                 solution = nums[counter]+nums[i]+nums[i+1]
@@ -11,18 +22,24 @@ def closer(nums, target):
             else:
                 break
         if counter <= len(nums)+2:
-            break
+             counter+= 1
+            
         else:
-            counter+= 1
+            break
     summ.append(target)
     summ.sort()
     index = summ.index(target)
 
     difference1 = abs(target-summ[index-1])
     difference2 = abs(target-summ[index+1])
-    if difference1> difference2:
+    if difference1<difference2:
         return summ[index-1]
     else:
         return summ[index+1]
+
+  
+#Testing case for the function
+
+# test1
+
     
-        
